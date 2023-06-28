44b211ff474348a7de042dc87749c47dd853a8c4
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-06-27 22:43:17 -0300

    Commit de prova para o futuro merge

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log('Oi');
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/hello', () => {
+
+    return 'PORRRAAA';
+});
+
+app.listen({
+    port: 3333,
+}).then( () => {
+    console.log('HTTP server running on port 3333')
+});

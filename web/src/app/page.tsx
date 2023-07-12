da41525840b7dd16d145a81d9f998d16d5c5174b
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-07-11 21:55:41 -0300

    Commit da segunda aula

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,19 @@
 
 export default function Home() {
-  return (<h1 className="h-screen bg-zinc-950 p-6 text-zinc-50">
+  return (<main className="grid grid-cols-2 min-h-screen">
+    {/* Left */}
+    <div className="flex flex-col items-start justify-between px-28 py-16 relative">
+<div className="absolute right-8 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50"></div>
+    </div>
 
-<h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-
-  </h1>)
+    {/* Right*/}
+    <div className="flex flex-col p-16">
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-center leading-relaxed w-[360px]">
+          Voce ainda não registrou nenhuma lembrança, comece a {' '}
+        </p>
+      </div>
+      <a href="" className="underline hover:text-gray-50">criar agora</a>
+    </div>
+  </main>)
 }

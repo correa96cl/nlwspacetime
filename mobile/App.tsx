3620263224b5347fe003b5609d72cc7aca4869a2
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-07-14 22:31:46 -0300

    Commit

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 
@@ -25,14 +25,21 @@ export default function App() {
   return (
     <ImageBackground imageStyle={{
       position: 'absolute', left: '-100%'
-    }} source={blurBg} className="bg-gray-900 flex-1 items-center relative">
-      <Text className='text-gray-50 font-alt font-bold text-5xl'>Vinicio !!!</Text>
+    }} source={blurBg} className="px-8 py-10 bg-gray-900 flex-1 items-center relative">
+      <Text className='text-gray-50 font-alt font-bold text-5xl'></Text>
       <StatusBar style="light" translucent />
       <StyledStripes className='absolute left-2' />
       <View className='flex-1 items-center justify-center gap-6'>
         <NLWLogo />
-        <Text>Sua capsula do tempo</Text>
+        <View className='space-y-2'>
+          <Text className='text-center font-title text-2xl text-gray-50'>Sua capsula do tempo</Text>
+          <Text className='text-center fomnt-body text-base leading-relaxed text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
+        </View>
+        <TouchableOpacity activeOpacity={0.7} className='rounded-full bg-green-500 px-5 py-3'>
+          <Text className='font-alt text-sm uppercase text-black'>Cadastrar lembrança</Text>
+        </TouchableOpacity>
       </View>
+      <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>Feito com no NLW da Rocketseat</Text>
     </ImageBackground>
 
   );
